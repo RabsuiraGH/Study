5963295c6a522236b164229d4428f6720272a631
RabsuiraGH <rabsuira@proton.me>
2024-08-15 13:51:36 +0300

    Ok with '#' will be ignored, and an empty message aborts the commit.

---
+++ LeetCode/141-linked-list-cycle/Program.cs
@@ -0,0 +1,76 @@
+﻿/*
+ Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+Example 1:
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+Example 2:
+
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+Example 3:
+
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
+
+Constraints:
+
+The number of the nodes in the list is in the range [0, 104].
+-105 <= Node.val <= 105
+pos is -1 or a valid index in the linked-list.
+
+Follow up: Can you solve it using O(1) (i.e. constant) memory?
+ */
+
+bool HasCycle(ListNode head)
+{
+    HashSet<ListNode> visited = new HashSet<ListNode>();
+
+    while (head != null)
+    {
+        if (visited.Contains(head)) return true;
+
+        visited.Add(head);
+        head = head.next;
+    }
+    return false;
+}
+
+bool HasCycle_2(ListNode head)
+{
+    //turtle and rabbit game
+    if (head == null) return false;
+
+    ListNode fastPointer = head, slowPointer = head;
+
+    while (fastPointer != null && fastPointer.next != null)
+    {
+        fastPointer = fastPointer.next.next;
+        slowPointer = slowPointer.next;
+        if (slowPointer == fastPointer)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x)
+    {
+        val = x;
+        next = null;
+    }
+}
