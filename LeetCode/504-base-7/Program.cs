4a1810c8561620f8bf36cce3866072b6c782ed16
RabsuiraGH <rabsuira@proton.me>
2024-08-26 20:29:55 +0300

    Base 7

---
+++ LeetCode/504-base-7/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Given an integer num, return a string of its base 7 representation.
+
+Example 1:
+
+Input: num = 100
+Output: "202"
+Example 2:
+
+Input: num = -7
+Output: "-10"
+
+Constraints:
+
+-107 <= num <= 107
+ */
+
+using System.Text;
+
+string ConvertToBase7(int num)
+{
+    if (num is > -7 and < 7) return num.ToString();
+
+    StringBuilder output = new();
+    int reminder = num;
+
+    if (reminder < 0)
+    {
+        reminder = Math.Abs(reminder);
+    }
+
+    while (reminder != 0)
+    {
+        output.Insert(0, reminder % 7);
+        reminder = reminder / 7;
+    }
+    if (num < 0)
+        output.Insert(0, '-');
+    return output.ToString();
+}
